bfb81025860735e200a4acec5c691557382fe6fe
Nikita Lapin <nikita.lapin.201248@gmail.com>
2018-05-11 16:25:48 +0300

    DataBase connection. Making template of Activity.

--- app/src/main/java/com/example/nikit/youthsongs/MainActivity.kt
+++ app/src/main/java/com/example/nikit/youthsongs/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.nikit.youthsongs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import com.example.nikit.youthsongs.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
     }
 }
